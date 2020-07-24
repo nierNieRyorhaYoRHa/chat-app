0e8d236c64355b81eba2fbd5b514daad3022f986
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-07-24 17:57:43 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,25 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
 end
+
 
 # チャットルームの新規作成なので「new」アクションを定義します。
 # また、form_withに渡す引数として、値が空のRoomインスタンスを@roomに代入しています。
+
+# この中に、user_ids: []という記述があります。このように、配列に対して保存を許可したい場合は、キーの名称と関連づけてバリューに「[]」と記述します。
